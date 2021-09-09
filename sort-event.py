2db1ded357b80f53d45a769796ad363763a784de
yukiue <45696312+yukiue@users.noreply.github.com>
2021-09-09 20:06:20 +0900

    日付順でソートした開催通知の一覧を表示するプログラムを追加した

---
+++ sort-event.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+import re
+
+from redminelib import Redmine
+
+SERVER_URL = 'https://rm.lsnl.jp/'
+API_ACCESS_KEY = os.getenv('REDMINE_API_ACCESS_KEY')
+
+redmine = Redmine(SERVER_URL, key=API_ACCESS_KEY)
+
+# get issues on event notification
+issues = redmine.issue.filter(tracker_id=5, status_id='open')
+
+events = []
+
+for issue in issues:
+    subject = issue.subject
+    project = issue.project
+    # status = issue.status
+    _id = issue.id
+
+    # extract end time of event from subject
+    regex = re.compile(
+        r'(\d{4})-(\d{2})-(\d{2})\s(\d{2}):(\d{2})-(\d{2}):(\d{2})')
+    mo = regex.search(subject)
+    if mo is not None:
+        events.append(f'{subject} {project} {_id}')
+
+for event in sorted(events):
+    print(event)
